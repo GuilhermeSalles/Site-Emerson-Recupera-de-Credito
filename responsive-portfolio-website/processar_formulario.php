ac66fbbae701e10a915b3db37906424438945a5a
GuilhermeSalles <guibaltazarvs@gmail.com>
2023-12-28 19:55:00 -0300

    pastas alteradas

---
+++ responsive-portfolio-website/processar_formulario.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["user_name"];
+    $email = $_POST["user_email"];
+    $desc = $_POST["user_project"];
+
+    // E-mail de destino
+    $to = "guibaltazarvs2@gmail.com";
+
+    // Assunto do e-mail
+    $subject = "Novo formulário de contato/orçamento";
+
+    // Mensagem do e-mail
+    $message = "Nome: $name\n";
+    $message .= "E-mail: $email\n";
+    $message .= "Telefone: $tel\n";
+    $message .= "Descrição:\n$desc";
+
+    // Cabeçalhos do e-mail
+    $headers = "From: $email";
+
+    // Enviar e-mail
+    mail($to, $subject, $message, $headers);
+     // Enviar e-mail
+     if (mail($to, $subject, $message, $headers)) {
+        // Redirecionar para a tela index.php
+        header("Location: index");
+        exit();  // Certifique-se de sair após o redirecionamento
+    } else {
+        echo "Erro ao enviar o e-mail.";
+    }
+}
+?>
